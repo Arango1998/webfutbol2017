c93c4f39b1cb3a9e58f334ee2065c599c243f6ef
Cristian Suesca <Cristian Suesca@10.1.237.144>
2017-03-21 07:58:06 -0500

    Segundo commit, se hace persistencia en categrias

---
+++ src/java/com/webfutbol2017/frontend/utilities/converters/AbstractConverter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webfutbol2017.frontend.utilities.converters;
+import javax.faces.convert.Converter;
+/**
+ *
+ * @author Cristian Suesca
+ */
+public abstract class AbstractConverter implements Converter{
+
+    protected String nameManagedBean;
+    
+    
+    
+    
+}
