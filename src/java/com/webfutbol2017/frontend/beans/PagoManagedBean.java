70c6e7e5a6b1f07c4855fbba3fc3cd83080cf20e
APRENDIZ <APRENDIZ@BOGDFPRDC8F421.FormacionSena.RED>
2017-03-23 10:40:08 -0500

    bahhh

--- src/java/com/webfutbol2017/frontend/beans/PagoManagedBean.java
+++ src/java/com/webfutbol2017/frontend/beans/PagoManagedBean.java
@@ -7,6 +7,8 @@ package com.webfutbol2017.frontend.beans;
 
 import com.webfutbol2017.backend.persistence.entities.Pago;
 import com.webfutbol2017.backend.persistence.facades.PagoFacade;
+import com.webfutbol2017.frontend.utilities.converters.InterfaceController;
+import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -19,7 +21,7 @@ import javax.enterprise.context.RequestScoped;
  */
 @Named(value = "pagoManagedBean")
 @RequestScoped
-public class PagoManagedBean {
+public class PagoManagedBean implements Serializable, InterfaceController<Pago> {
 
     /**
      * Creates a new instance of PagoManagedBean
@@ -59,4 +61,11 @@ public class PagoManagedBean {
 
         return pagof.findAll();
     }
+
+    @Override
+    public Pago getObjectByKey(Integer key) {
+    return pagof.find(key);
+    }
+
+    
 }
